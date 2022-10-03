41b7aae85d9193c62c7e3a53431f8f550dfbe42b
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-10-03 10:29:40 +0900

    1_1_ 改行

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
-template <typename Type1>
-Type1 Min(Type1 a, Type1 b) {
+
+template <typename Type>
+Type Min(Type a, Type b) {
 	if (a >= b) {
-		return static_cast<Type1>(a);
+		return static_cast<Type>(a);
 	}
 
-	return static_cast<Type1>(b);
+	return static_cast<Type>(b);
 }
 template <>
 char Min<char>(char a, char b) {
@@ -14,6 +15,7 @@ char Min<char>(char a, char b) {
 }
 
 int main() {
+
 	printf("%d\n", Min<int>(114, 51));
 	printf("%f\n", Min<float>(11.4f, 51.4f));
 	printf("%f\n", Min<double>(11.4, 51.4));
