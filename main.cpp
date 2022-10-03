09f90556909d7d94d70444d1403e7ca342db2c51
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-10-03 09:38:09 +0900

    1_1_資料と同じ

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
+template <typename Type1, typename Type2>
+Type1 add(Type1 a, Type2 b) {
+	return a + b;
+}
 
+template <>
+float add<float>(float a, float b) {
+	return a - b;
+}
+
+float add(float a, float b) {
+	return a * b;
+}
 
 int main() {
-
-
+	printf("%d\n", add<int>(114, 51.4f));
+	printf("%f\n", add<float>(11.4f, 51.4f));
+	printf("%f\n", add(11.4f, 51.4f));
 
 	return 0;
 }
