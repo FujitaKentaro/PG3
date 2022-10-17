2487e8c5419da7e659877e00cb5affb2e0f92a5b
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-10-17 16:27:15 +0900

    動作はできた

--- main.cpp
+++ main.cpp
@@ -1,37 +1,67 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <time.h>
+#include <stdlib.h>
 
-int RecursivePay(int time) {
-	if (time <= 1) {
-		return 100;
-	}
+typedef void (*PFunc)(int*);
 
-	return (RecursivePay(time - 1) * 2 - 50);
-}
+// 関数
+void Callback(int* num);
+void SetTimeout(int second);
 
-int RecursiveResult(int time) {
-	int result = 0;
-	for (int i = 1; i <= time; i++) {
-		result += RecursivePay(i);
-	}
-	return result;
-}
-int GeneralPay(int time) {
-	if (time <= 0) {
-		return 0;
-	}
-	return 1072 * time;
-}
+// ユーザー関係
+void SetPlayerNum(int* num);
 
 int main() {
-	int time = 1;
-	while (true)
-	{
-		if (RecursiveResult(time) > GeneralPay(time)) {
-			printf("\n %d 時間働くと %d円で 再帰的 の勝ち\n", time, RecursiveResult(time));
-			break;
-		}
-		time++;
-	}
+	srand(time(nullptr));
+
+	SetTimeout(3000); // 3000ミリ秒？
 
 	return 0;
+}
+
+// 関数定義
+void Callback(int* num) {
+	int result;
+	result = rand() % 12 + 1;
+	// ランダムな値を返す
+	printf(" 出目：%d\n", result);
+
+	if (result % 2 == 0 && *num % 2 == 0 ||
+		result % 2 == 1 && *num % 2 == 1) {
+		printf("\n 正解 \n");
+	}
+	else {
+		printf("\n 不正解 \n");
+	}
+}
+
+void SetTimeout(int second) {
+	PFunc p;
+	int num;
+	p = SetPlayerNum;
+	p(&num);
+	// 待機時間
+	Sleep(second);
+	// コールバック関数
+	p = Callback;
+	p(&num);
+}
+
+// 数の入力
+void SetPlayerNum(int* num) {
+
+	printf("値を入力してください\n\n");
+	scanf_s("%d", num);
+	if (*num % 2 == 0) {
+		printf("偶数が入力されました\n");
+		*num = 2;
+	}
+	else if (*num % 2 == 1) {
+		printf("奇数が入力されました\n");
+		*num = 1;
+	}
+	else {
+		printf("予期せぬ値が入力されました\n");
+	}
 }
