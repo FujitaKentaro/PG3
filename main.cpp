3d533807852a5a2f67a7f8b91feb0356f605606a
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-11-14 09:10:58 +0900

    編集ブランチ修正

--- main.cpp
+++ main.cpp
@@ -6,15 +6,14 @@
 
 using namespace std;
 
-// 関数
-void SetTimeout(int second, std::function<void(int*)> PlNum, std::function<void(int*)> Corect);
-
-int main(int argc ,const char *argv[]) {
+int main(int argc, const char* argv[]) {
 
 	srand(time(nullptr));
 
 	int waitTime = 3;
 
+
+	// ラムダ式
 	std::function<void(int*)> PlayerNum = [](int* num) {
 		printf("値を入力してください\n\n");
 		scanf_s("%d", num);
@@ -30,32 +29,30 @@ int main(int argc ,const char *argv[]) {
 			printf("予期せぬ値が入力されました\n");
 		}
 	};
-	std::function<void(int*)> Correct = [](int* num) {
+	std::function<void(int, std::function<void(int*)>)> SetTimeOut = [](int second, std::function<void(int*)> PlNum) {
 		int result;
+		int num;
+		PlNum(&num);
+
+		// 待機時間
+		Sleep(second * 1000);
+
 		result = rand() % 12 + 1;
 		// ランダムな値を返す
 		printf(" 出目：%d\n", result);
-		if (result % 2 == 0 && *num % 2 == 0 ||
-			result % 2 == 1 && *num % 2 == 1) {
+		if (result % 2 == 0 && num % 2 == 0 ||
+			result % 2 == 1 && num % 2 == 1) {
 			printf("\n 正解 \n");
 		}
 		else {
 			printf("\n 不正解 \n");
-		}		
+		}
+
 	};
 
-	SetTimeout(waitTime,PlayerNum,Correct); 
+
+	// 実行
+	SetTimeOut(waitTime, PlayerNum);
 
 	return 0;
-}
-
-// 関数定義
-
-void SetTimeout(int second, std::function<void(int*)> PlNum, std::function<void(int*)> Corect) {
-	int num;
-	PlNum(&num);
-	// 待機時間
-	Sleep(second * 1000);
-	// コールバック関数
-	Corect(&num);
 }
