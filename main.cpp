36d7b9f8f583edb65a88d6dc8acb3456390059aa
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-12-01 07:58:03 +0900

    0202_7p

--- main.cpp
+++ main.cpp
@@ -1,57 +1,49 @@
 #include <stdio.h>
-#include <Windows.h>
-#include <time.h>
 #include <stdlib.h>
-#include <functional>
-#include <vector>
+// 手順1 単方向リストの構造体の定義
+typedef struct CELL {
+	int val;
+	struct CELL* next;
+} CELL;
 
-#include <iostream>
-#include <list>
+void create(CELL* cell,int val);	// データを追加する関数のプロトタイプ宣言
+void index(CELL* cell);	// 一覧を表示する関数のプロトタイプ宣言
 
-using namespace std;
 
 int main() {
+	int val;
+	// 2 先頭のセルの宣言
+	CELL head;
+	head.next = nullptr;	// ヴィジュアルスタジオではNEXTに何らかの値が入った状態で初期化されるので、nullポインターを代入する
+	while (true){
+		scanf_s("%d", &val);
 
-	std::list<const char*> yamanote = {
-		"Tokyo","Kanda","Akihabara","Okatimati","Ueno",
-		"Uguisudani","Nippori","Tabata","Komagome","Sugamo",
-		"Ootuka","Ikebukuro","Mejiro","Takadanobaba","shinookubo",
-		"Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro",
-		"Gotanda","Oosaki","Shinagawa","Tamati","Hamamatutyou",
-		"Shinbasi","Yurakutyou"		
-	};
+		// 3 最後尾にセルを追加
+		create(&head, val);
 
-	std::cout << "1970s";
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
-	std::cout<< "\n";
-
-	std::cout << "2019s";
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		if (*itr == "Tabata") {
-			itr = yamanote.insert(itr, "Nishinippori");
-			++itr;
-		}
-	}
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
-	std::cout << "\n";
-
-	std::cout << "2022s";
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		
-		if (*itr == "Tamati") {
-			itr = yamanote.insert(itr, "Takanawagetowei");
-			++itr;
-		}
-	}
-	for (auto itr = yamanote.begin(); itr != yamanote.end(); ++itr) {
-		std::cout << *itr << "\n";
+		// 4 リスト一覧の表示
+		index(&head);
 	}
 
 	system("pause");
 
 	return 0;
+}
+
+void create(CELL* cell, int val) {
+	CELL* newCell;
+	// 新規作成するセル分のメモリを確保する
+	newCell = (CELL*)malloc(sizeof(CELL));
+	newCell->val = val;
+	newCell->next = nullptr;
+
+	// 最後（最新）のセルのアドレスの一つ目の処理は引数から持ってきた
+	// リストのうち最初のセルのアドレスが該当する
+	while ()
+	{
+
+	}
+}
+void index(CELL* cell) {
+
 }
