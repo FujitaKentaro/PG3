a7f0283fb375feee20223c2ae08636f279bfabdd
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2023-01-23 09:11:12 +0900

    行削減

--- main.cpp
+++ main.cpp
@@ -12,28 +12,6 @@ int main(int argc, const char* argv[]) {
 
 	int waitTime = 3;
 
-
-	// ラムダ式
-	std::function<void(int*)> PlayerNum = [](int* num) {
-		int result;
-		result = rand() % 12 + 1;
-		// ランダムな値を返す
-		printf(" 出目：%d\n", result);
-		if (result % 2 == 0 && *num % 2 == 0 ||
-			result % 2 == 1 && *num % 2 == 1) {
-			printf("\n 正解 \n");
-		}
-		else {
-			printf("\n 不正解 \n");
-		}
-	};
-	std::function<void(int*, std::function<void(int*)>)> SetTimeOut = [](int* num, std::function<void(int*)> PlNum) {
-
-		// 待機時間
-		Sleep(3 * 1000);
-
-		PlNum(num);
-	};
 	int num;
 
 	printf("値を入力してください\n\n");
@@ -50,8 +28,36 @@ int main(int argc, const char* argv[]) {
 		printf("予期せぬ値が入力されました\n");
 	}
 
+
+	// ラムダ式
+	std::function<void()> PlayerNum = [=]() {
+		int result;
+		result = rand() % 12 + 1;
+		// ランダムな値を返す
+		printf(" 出目：%d\n", result);
+		if (result % 2 == 0 && num % 2 == 0 ||
+			result % 2 == 1 && num % 2 == 1) {
+			printf("\n 正解 \n");
+		}
+		else {
+			printf("\n 不正解 \n");
+		}
+	};
+	std::function<void(std::function<void()>, int)>setTimeout = [=](std::function<void()> plNum, int num) {
+		Sleep(num * 1000);
+		plNum();
+	};
+	//std::function<void(int*, std::function<void(int*)>)> SetTimeOut = [](int* num, std::function<void(int*)> PlNum) {
+
+	//	// 待機時間
+	//	Sleep(3 * 1000);
+
+	//	PlNum(num);
+	//};
+	
+
 	// 実行
-	SetTimeOut(&num, PlayerNum);
+	setTimeout(PlayerNum,num);
 
 	system("pause");
 
