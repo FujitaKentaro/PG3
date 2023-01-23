6859d912031b77980323beaa1b2f422a40263296
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2023-01-23 09:29:48 +0900

    0301

---
+++ SceneManager.cpp
@@ -0,0 +1,56 @@
+#include "SceneManager.h"
+#include <stdio.h>
+
+SceneManager::SceneManager() {}
+
+SceneManager::~SceneManager() {}
+
+
+SceneManager* SceneManager::GetInstance()
+{
+	// ŠÖ”“àstatic•Ï”‚Æ‚µ‚ÄéŒ¾‚·‚é
+	static SceneManager instance;
+
+	return &instance;
+}
+
+void SceneManager::ChangeScene(int sceneNo)
+{
+	switch (sceneNo)
+	{
+	case 0:
+		TitleScene();
+		break;
+	case 1:
+		NewGameScene();
+		break;
+	case 2:
+		GamePlayScene();
+		break;
+	case 3:
+		GameClear();
+		break;
+	default:
+		break;
+	}
+}
+
+void SceneManager::TitleScene() {
+	printf("SceneNo:0\n");
+	printf("\n TITLE \n\n");
+}
+
+void SceneManager::NewGameScene() {
+	printf("SceneNo:1\n");
+	printf("\n GAME SCENE 1 \n");
+}
+
+void SceneManager::GamePlayScene() {
+	printf("SceneNo:2\n");
+	printf("\n GAME SCENE 2 \n");
+}
+
+void SceneManager::GameClear() {
+	printf("SceneNo:3\n");
+	printf("\n GAME CLEAR \n");
+}
--- main.cpp
+++ main.cpp
@@ -1,85 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// 手順1 単方向リストの構造体の定義
-typedef struct CELL {
-	int val;
-	struct CELL* prev;
-	struct CELL* next;
-} CELL;
-
-void create(CELL* cell,int val);	// データを追加する関数のプロトタイプ宣言
-void index(CELL* cell);	// 一覧を表示する関数のプロトタイプ宣言
-CELL* getInsertCellAddress(CELL* endCELL,int iterator);
-
+#include <Windows.h>
+#include "SceneManager.h"
 
 int main() {
-	int iterator;
-	int inpitValue;
-	CELL* insertCell;
 
-	// 2 先頭のセルの宣言
-	CELL head;
-	head.next = nullptr;	// ヴィジュアルスタジオではNEXTに何らかの値が入った状態で初期化されるので、nullポインターを代入する
-	while (true){
-		printf("何番目のセルの後ろに挿入しますか？\n");
-		scanf_s("%d",&iterator);
+	SceneManager* sceneManager = SceneManager::GetInstance();
+	int num = 0;
 
-		printf("挿入する値を入力してください\n");
-		scanf_s("%d", &inpitValue);
+	while (true) {
 
-		// 3 任意のセルのうしろに追加
-		insertCell = getInsertCellAddress(&head,iterator);
-		create(insertCell, inpitValue);
+		if (num == 3) {
+			num = 0;
+		}
+		else if(num >= 4){
 
-		// 4 リスト一覧の表示
-		index(&head);
+			break;
+		}
+
+		sceneManager->ChangeScene(num);
+
+		num++;
+
+		Sleep(1.5 * 1000);
 	}
 
 	system("pause");
-
 	return 0;
-}
 
-void create(CELL* cell,int val) {
-	CELL* newCell;
-	// 新規作成するセル分のメモリを確保する
-	newCell = (CELL*)malloc(sizeof(CELL));
-	newCell->val = val;
-	newCell->prev = cell;
-	newCell->next = cell->next;
-
-	if (cell->next) {
-		CELL* nextCell = cell->next;
-		nextCell->prev = newCell;
-	}
-	cell->next= newCell;
-}
-
-void index(CELL* endCell) {
-	int no = 1;
-	while (endCell->next!=nullptr)
-	{
-		endCell = endCell->next;
-		printf("%d:no\n", no);
-		printf(" %p\n", endCell->prev);
-		printf(" %5d\n", endCell->val);
-		printf(" (%p)\n", endCell);
-		printf(" %p\n", endCell->next);
-		no++;
-	}
-}
-
-CELL* getInsertCellAddress(CELL* endCELL, int iterator) {
-	for (int i = 0; i < iterator; i++) {
-		if (endCELL->next) {
-			endCELL = endCELL->next;
-		}
-		else
-		{
-			break;
-		}
-	}
-	return endCELL;
 }
